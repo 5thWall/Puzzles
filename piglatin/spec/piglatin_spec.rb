6cf3784f43b02595778070fdcf32a163ae6add74
Andy <5thWall@gmail.com>
2011-10-28 22:01:10 -0700

    Expanded vowel test to be comprehensive.

--- piglatin/spec/piglatin_spec.rb
+++ piglatin/spec/piglatin_spec.rb
@@ -4,12 +4,15 @@ describe "Pig Latin" do
   describe "#transform_word" do
     it "should move consonants before the first vowel to the back and add 'ay'" do
       PigLatin.transform_word("pony").should == "onypay"
-      PigLatin.transform_word("fluttershy").should == "uttershyflay"
+      PigLatin.transform_word("bridle").should == "idlebray"
     end
     
     it "should add 'way' to the end of words that start with a vowel" do
-      PigLatin.transform_word("unicorn").should == "unicornway"
-      PigLatin.transform_word("applejack").should == "applejackway"
+      PigLatin.transform_word("apples").should == "applesway"
+      PigLatin.transform_word("equine").should == "equineway"
+      PigLatin.transform_word("icing").should == "icingway"
+      PigLatin.transform_word("owl").should == "owlway"
+      PigLatin.transform_word("uncommon").should == "uncommonway"
     end
   end
 end
