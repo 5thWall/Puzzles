d9acd25f48b8115c5a4d792857b4ffa9c81dfa24
Andy <5thWall@gmail.com>
2011-10-28 22:40:18 -0700

    Now honors capitalized words.

--- piglatin/lib/piglatin.rb
+++ piglatin/lib/piglatin.rb
@@ -1,8 +1,10 @@
 module PigLatin
   def transform_word word
+    is_cap = word.chr.match /[A-Z]/
     word.match /([^aeiou]*)(\w*)/i
-    first = $1.empty? ? 'w' : $1
-    "#{$2}#{first}ay"
+    first = $1.empty? ? 'w' : $1.downcase
+    second = is_cap ? $2.capitalize : $2
+    "#{second}#{first}ay"
   end
   
   module_function :transform_word
